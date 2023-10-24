7cda1fdea4cc8f30f48ec9eae7a3acfcded24d08
Emilio Villa <villa.emilio1994@gmail.com>
2023-10-24 16:41:18 +0200

    Update dashboard
    - Add histogram of residuals
    - Add table with metrics

--- dashboard/assets/callbacks.py
+++ dashboard/assets/callbacks.py
@@ -7,8 +7,11 @@ def get_callbacks(app):
     # Fit with polynomial of degree d where d is slider value
     @app.callback(
         Output("plot", "figure"),
+        Output("plot-residuals", "figure"),
+        Output("table", "data"),
         Input("slider", "value"),
         prevent_initial_call=True
     )
     def fit_with_polynomial_of_degree_d(d):
-         return utils.make_plot(constants.x, constants.y, degree=d)
+         plot, residuals, data = utils.make_plots(constants.x, constants.y, degree=d)
+         return plot, residuals, data
--- dashboard/main_app.py
+++ dashboard/main_app.py
@@ -1,5 +1,5 @@
 from assets import callbacks, constants, utils
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table
 import dash_bootstrap_components as dbc
 
 # Set app style
@@ -22,11 +22,52 @@ app.layout = dbc.Container([
         dbc.CardBody([
             dcc.Markdown("""Use the slider below to select the degree of the fitting polynomial."""),
             dbc.Spinner(
-                dcc.Graph(id='plot', figure=utils.make_plot(constants.x, constants.y), config=constants.gconfig),
+                dcc.Graph(id='plot', figure=utils.make_plots(constants.x, constants.y)[0],
+                          config=constants.gconfig),
             color="primary"),
-            dcc.Slider(1, 100, value=3, marks={1: 1, 3:3, 10: 10, 50:50, 100: 100}, id="slider")
+            dcc.Slider(1, 100, value=3, marks={1: 1, 3: 3, 10: 10, 50: 50, 100: 100}, id="slider")
         ])
-    )
+    ),
+
+    # Plot residuals and add table with metrics
+    dbc.Row(
+      [
+          dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    dbc.Spinner(
+                        dcc.Graph(id='plot-residuals', figure=utils.make_plots(constants.x, constants.y)[1],
+                                  config=constants.gconfig),
+                    color="primary"),
+                ])
+            , style={"height": "100%"})
+          ),
+          dbc.Col(
+              dbc.Card(
+                  dbc.CardBody([
+                      html.Div(
+                          children=[
+                              dash_table.DataTable(
+                                  id='table',
+                                  columns=(
+                                      {"id": "Metric", "name": "Metric", "type": "text"},
+                                      {"id": "Value", "name": "Value", "type": "number"},
+                                  ),
+                                  data=utils.make_plots(constants.x, constants.y)[2],
+                                  style_cell={
+                                      'overflow': 'hidden',
+                                      'textOverflow': 'ellipsis',
+                                      'font-family': 'sans-serif',
+                                      'textAlign': 'left'
+                                  },
+                                  editable=False,
+                              )]
+                      )
+                  ], style={"align": "center"})
+              , style={"height": "100%"})
+          ),
+      ]
+    , className="g-0")
 
 ])
 
